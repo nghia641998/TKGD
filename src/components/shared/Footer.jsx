206023ee88d0055dbca4917ca3cf16ee79dd1c53
diendh <diendh@vng.com.vn>
2019-12-18 22:30:47 +0700

    fix css in homepage, header, footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className='footer-logo'>
+    <div className='footer row'>
+      <div className='footer-logo col-md-3 col-sm-6 col-12'>
         <div className='logo'>Logo</div>
         <div className='footer-icon'>
           <i className='fab fa-instagram' />
@@ -12,7 +12,7 @@ const Footer = () => {
           <i className='fas fa-globe-europe' />
         </div>
       </div>
-      <div className='footer-info'>
+      <div className='footer-info col-md-3 col-sm-6 col-12'>
         <div className='footer-info-title'>CAREER RESOURCES</div>
         <div className='footer-info-content'>How to Write a Resume</div>
         <div className='footer-info-content'>
@@ -22,13 +22,13 @@ const Footer = () => {
         <div className='footer-info-content'>That Lands You an Interview</div>
         <div className='footer-info-content'>Blog</div>
       </div>
-      <div className='footer-info'>
+      <div className='footer-info col-md-3 col-sm-6 col-12'>
         <div className='footer-info-title'>OUR COMPANY</div>
         <div className='footer-info-content'>About Us</div>
         <div className='footer-info-content'>Pricing</div>
         <div className='footer-info-content'>Product Updates</div>
       </div>
-      <div className='footer-info'>
+      <div className='footer-info col-md-3 col-sm-6 col-12'>
         <div className='footer-info-title'>SUPPORT</div>
         <div className='footer-info-content'>FAQ</div>
         <div className='footer-info-content'>Contact Us</div>
