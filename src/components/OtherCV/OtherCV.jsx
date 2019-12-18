119500871f8a8480613cb154c1e865abc9725101
1612421 <nguyenngocnghia53@gmail.com>
2019-12-19 00:11:22 +0700

    fix UI responsice other cv

--- src/components/OtherCV/OtherCV.jsx
+++ src/components/OtherCV/OtherCV.jsx
@@ -31,20 +31,14 @@ class OtherCV extends React.Component {
     const { isShowModal } = this.state;
 
     return (
-      <div className="other-cv pt-5">
+      <div className="other-cv pt-5 col-12">
         <div className="d-flex">
           <div className="header-banner">
             <div className="header-title">Find The Perfect</div>
             <div className="header-title">Employee For Your Business</div>
-            <div className="search-wrapper">
-              <div id='Component_2___1' className='Component_2___1'>
-                <svg className='ic_search_24px' viewBox='3 3 28.582 29.475'>
-                  <path
-                    fill='rgba(164,164,164,1)'
-                    id='ic_search_24px'
-                    d='M 23.42772483825684 21.53753662109375 L 22.13669586181641 21.53753662109375 L 21.67911338806152 21.08252334594727 C 23.28064727783203 19.1613597869873 24.24483489990234 16.6672191619873 24.24483489990234 13.95399856567383 C 24.24483489990234 7.904021263122559 19.48926162719727 3.000000238418579 13.6224193572998 3.000000238418579 C 7.755575180053711 3.000000238418579 3.000000238418579 7.904021263122559 3.000000238418579 13.95399856567383 C 3.000000238418579 20.00397491455078 7.755575180053711 24.90799903869629 13.6224193572998 24.90799903869629 C 16.25350952148438 24.90799903869629 18.67215347290039 23.91371154785156 20.53516006469727 22.26218795776367 L 20.97640037536621 22.73405265808105 L 20.97640037536621 24.06538200378418 L 29.14748764038086 32.47468566894531 L 31.58247375488281 29.96368789672852 L 23.42772483825684 21.53753662109375 Z M 13.6224193572998 21.53753662109375 C 9.553215980529785 21.53753662109375 6.268435955047607 18.15022277832031 6.268435955047607 13.95399856567383 C 6.268435955047607 9.75777530670166 9.553215980529785 6.370460510253906 13.6224193572998 6.370460510253906 C 17.69162178039551 6.370460510253906 20.97640037536621 9.75777530670166 20.97640037536621 13.95399856567383 C 20.97640037536621 18.15022277832031 17.69162178039551 21.53753662109375 13.6224193572998 21.53753662109375 Z'
-                  />
-                </svg>
+            <div className="row col-12 mt-2">
+              <div className='search-control min-width col-8'>
+                <i className="fas fa-search ml-2"/>
                 <div id='Type_name_template_here____'>
                   <input
                     className='custom-input-text'
@@ -55,20 +49,20 @@ class OtherCV extends React.Component {
                   />
                 </div>
               </div>
-              <Link to="/category/all/all" className="btn btn-search">
-              Search
+              <Link to="/category/all/all" className="btn btn-search col-2">
+                Search
               </Link>
               
             </div>
-            <div className="hash-tag">
-              <div>Popular: </div>
-              <Link to="/category/all/all" className="btn btn-outline-secondary">
+            <div className="d-flex flex-wrap align-items-center mt-sm-2 hash-tag">
+              <div className='mr-2'>Popular: </div>
+              <Link to="/category/all/all" className="btn btn-outline-secondary mr-1 mt-1">
                 Developer
               </Link>
-              <Link to="/category/all/all" className="btn btn-outline-secondary">
+              <Link to="/category/all/all" className="btn btn-outline-secondary mr-1 mt-1">
               React
               </Link>
-              <Link to="/category/all/all" className="btn btn-outline-secondary">
+              <Link to="/category/all/all" className="btn btn-outline-secondary mr-1 mt-1">
               Angular
               </Link>
             </div>
@@ -91,7 +85,7 @@ class OtherCV extends React.Component {
               </div>
             </div>
           </div>
-          <div className="header-banner-right">
+          <div className="header-banner-right d-none  d-lg-block col-lg-3">
             <img src="/images/exampleCV.png" alt="example cv" />
             <div className="d-flex stars">
               <i className="fas fa-star" />
@@ -121,7 +115,7 @@ class OtherCV extends React.Component {
           {CV.map((item, index) => {
             return (
               <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4" key={index}>
-                <div className="image-cv">
+                <div className="image-cv mx-auto  my-auto">
                   <img alt="template" src={item} />
                   <div className="template-hover">
                     <div className="d-flex flex-column x-index-5">
